5a60f05b6c0b8041a0f85678f505721ff4ec31e1
Md zihad khan <54374227+rainbowseven@users.noreply.github.com>
2019-08-24 00:33:40 +0600

    2nd project

--- js/main.js
+++ js/main.js
@@ -1,27 +1,154 @@
 $(function () {
-    var mixer = mixitup('.filter_img');
+
+    var $menu = $('.navbar').offset().top;
+
+    $(window).scroll(function () {
+        var $scrolling = $(this).scrollTop();
+
+        if ($scrolling >= $menu) {
+
+            $('.navbar').addClass('stricky')
+           
+        }
+        else{
+            $('.navbar').removeClass('stricky')
+        }
+    })
+
+
+
+
+//    video section venobox
+    
+    
+$('.venobox').venobox(); 
+    
+//    ==========
+
+
 
     $('#banner').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
         arrows: false,
+        speed: 300,
         autoplay: true,
         dots: true,
+        autoPlay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+   
+  ]
     });
 
-    $('#service').parallax({
-        imageSrc: '/imagrs/service.png'
-    });
 
-    
-//    service slider
-    
-    $('.service').slick({
+    //    screenshorts slider
+
+    $('.slider_ss').slick({
         arrows: false,
-        infinite: true,
+        speed: 300,
         autoplay: true,
-        speed: 800,
+        dots: true,
+        autoPlay: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+    
+  ]
+    });
+
+
+    //pricing slider
+
+
+
+    $('.main_pricing').slick({
+        arrows: false,
+        speed: 300,
+        autoPlay: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+   
+  ]
+    });
+
+
+    //    team member 
+    $('.team_slider').slick({
+        arrows: false,
+        speed: 300,
+        autoPlay: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
@@ -48,28 +175,30 @@ $(function () {
                     slidesToScroll: 1
                 }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
+
   ]
     });
-    
-    //   news slider
-    
-      
-    $('.news-slider').slick({
-        arrows: false,
+
+
+
+
+    //    feedbaack slidder 1
+
+    $('.feedback_slide').slick({
+        focusOnSelect: true,
         infinite: true,
-        autoplay: true,
-        speed: 800,
-        slidesToShow: 3,
+        arrows: false,
+        autoPlay: true,
+        speed: 300,
+        asNavFor: '.clients_slider',
+        slidesToShow: 1,
         slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
@@ -77,8 +206,8 @@ $(function () {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
     },
             {
@@ -93,4 +222,55 @@ $(function () {
     // instead of a settings object
   ]
     });
+
+
+    //    feedbaack slidder 2
+
+    $('.clients_slider').slick({
+        focusOnSelect: true,
+        infinite: true,
+        arrows: false,
+        autoPlay: true,
+        centerMode: true,
+        centerPadding: 0,
+        asNavFor: '.feedback_slide',
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+   
+  ]
+    });
+
+
+
+
+
+
+
+
+
 })
